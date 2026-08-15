83577d2c66de199a1201e71417f0a0695d501f73
VertinCode <2422476862@qq.com>
2026-08-15 23:50:51 +0800

    Add files via upload

---
+++ BoolCharSize.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+#include<stdbool.h>
+     int main() {
+         bool a;
+         char b;
+         printf("%lu\n",sizeof(a));
+         printf("%lu\n",sizeof(b));
+         return 0;
+     }  
